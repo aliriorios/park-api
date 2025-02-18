d3e679876c8a371ecae6ea2f847d5ab40bc4e103
Alirio Rios <alirioriosmc@gmail.com>
2025-02-18 11:38:18 -0300

    Inserindo configuracoes timezone e locale, e configurando banco de dados com docker

---
+++ docker/database/init-db.sql
@@ -0,0 +1,2 @@
+GRANT ALL PRIVILEGES ON 'park-api-db'.* TO 'alirio-dev'@'%' IDENTIFIED BY 'dev';
+FLUSH PRIVILEGES;
---
+++ src/main/java/br/com/example/park_api/config/SpringTimezoneConfig.java
@@ -0,0 +1,15 @@
+package br.com.example.park_api.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimezoneConfig {
+
+    @PostConstruct
+    public void TimezoneConfig () {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Bahia"));
+    }
+}
