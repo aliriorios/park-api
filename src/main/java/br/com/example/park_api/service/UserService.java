a6712be5a8411481b7c5d8986fbf9f571713c2dc
Alirio Rios <alirioriosmc@gmail.com>
2025-02-20 13:43:39 -0300

    Criando recurso de findById

--- src/main/java/br/com/example/park_api/service/UserService.java
+++ src/main/java/br/com/example/park_api/service/UserService.java
@@ -17,4 +17,11 @@ public class UserService {
     public User save(User user) {
         return userRepository.save(user);
     }
+
+    @Transactional(readOnly = true)
+    public User findById (Long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("User not founded.")
+        );
+    }
 }
--- src/main/java/br/com/example/park_api/web/controller/UserController.java
+++ src/main/java/br/com/example/park_api/web/controller/UserController.java
@@ -6,10 +6,7 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/users")
@@ -19,8 +16,14 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping(value = "/save")
-    public ResponseEntity<User> createUser (@RequestBody User user) {
+    public ResponseEntity<User> saveUser (@RequestBody User user) {
         User response = userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById (@PathVariable Long id) {
+        User response = userService.findById(id);
+        return ResponseEntity.ok(response);
     }
 }
