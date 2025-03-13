3ed1b9c7f798fff09ad9f0f95fce74d97878d0cc
Alirio Rios <alirioriosmc@gmail.com>
2025-03-13 11:26:46 -0300

    Categorizando recursos e documentando recurso de salvar usuarios individualmente

--- src/main/java/br/com/example/park_api/config/SpringDocOpenApiConfig.java
+++ src/main/java/br/com/example/park_api/config/SpringDocOpenApiConfig.java
@@ -14,7 +14,7 @@ public class SpringDocOpenApiConfig {
         return new OpenAPI().info(
                 new Info()
                         .title("REST API - Spring Park")
-                        .description("API para gestão de estacionamento de veículos")
+                        .description("API for vehicle parking management")
                         .version("v1")
                         .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
                         .contact(new Contact().name("Alírio Rios").email("contato.alirio@gmail.com"))
--- src/main/java/br/com/example/park_api/web/controller/UserController.java
+++ src/main/java/br/com/example/park_api/web/controller/UserController.java
@@ -6,9 +6,15 @@ import br.com.example.park_api.web.dto.UserCreateDto;
 import br.com.example.park_api.web.dto.UserResponseDto;
 import br.com.example.park_api.web.dto.UserUpdatePasswordDto;
 import br.com.example.park_api.web.dto.mapper.UserMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.spi.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,11 +24,20 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "api/v1/users")
 @RequiredArgsConstructor
+@Tag(name = "Users", description = "Contains all operations related to the resources for registering, editing and reading a user.")
 public class UserController {
     @NonNull
     private final UserService userService;
 
     @PostMapping(value = "/save")
+    @Operation(
+            summary = "Create a new user", description = "Feature to create a new user",
+            responses = {
+                @ApiResponse(responseCode = "201", description = "Successfully created resource", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
+                @ApiResponse(responseCode = "409", description = "User e-mail already registered in the system", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                @ApiResponse(responseCode = "422", description = "Resource Not Processed for Invalid Input Data", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     public ResponseEntity<UserResponseDto> saveUser (@Valid @RequestBody UserCreateDto dto) {
         User response = userService.save(UserMapper.toUser(dto));
         return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toResponseDto(response));
