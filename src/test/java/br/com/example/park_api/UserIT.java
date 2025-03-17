9d85375f7ec4cf7941e7e9b980b45bc9c7334e38
Alirio Rios <alirioriosmc@gmail.com>
2025-03-17 19:03:59 -0300

    Aplicando testes ponta a ponta na entidade de Users

--- src/test/java/br/com/example/park_api/UserIT.java
+++ src/test/java/br/com/example/park_api/UserIT.java
@@ -2,6 +2,7 @@ package br.com.example.park_api;
 
 import br.com.example.park_api.web.dto.UserCreateDto;
 import br.com.example.park_api.web.dto.UserResponseDto;
+import br.com.example.park_api.web.exception.ErrorMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,13 +20,13 @@ public class UserIT {
     // Como colocar nome nos métodos de teste
     //<motivo do teste> _ <o que vai ser testado> _ <o que será retornado>
 
-    @Test
+    @Test // Successfully test
     public void createUser_UsernameAndPasswordValidate_ReturnCreatedUserWithStatus201(){
         UserResponseDto responseBody = testClient
-                .post()
-                .uri("/api/v1/users/save")
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(new UserCreateDto("tody@gmail.com", "123456"))
+                .post() // http method
+                .uri("/api/v1/users/save") // method uri
+                .contentType(MediaType.APPLICATION_JSON) // response archive type
+                .bodyValue(new UserCreateDto("tody@gmail.com", "123456")) // request body
                 .exchange() // response
                 .expectStatus().isCreated() // status code 201
                 .expectBody(UserResponseDto.class) // tipo do objeto retornado
@@ -38,4 +39,111 @@ public class UserIT {
         org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENT");
     }
 
+    @Test // Username conflict test
+    public void createUser_UsernameDuplicateConflict_ReturnErrorMessageStatus409(){
+        // The username in this test already exist in test/resources/sql/users/users-insert.sql
+
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/users/save")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDto("ana@gmail.com", "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(409)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
+    }
+
+    @Test // Username validation test
+    public void createUser_InvalidUsername_ReturnErrorMessageStatus422(){
+        // Empty username
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/users/save")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDto("", "123456"))
+                .exchange() // response
+                .expectStatus().isEqualTo(422) // status code 422
+                .expectBody(ErrorMessage.class) // tipo do objeto retornado
+                .returnResult().getResponseBody(); // retorna um objeto do tipo UserResponseDto
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        // Invalid username format
+        responseBody = testClient
+                .post()
+                .uri("/api/v1/users/save")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDto("tody@", "123456"))
+                .exchange() // response
+                .expectStatus().isEqualTo(422) // status code 422
+                .expectBody(ErrorMessage.class) // tipo do objeto retornado
+                .returnResult().getResponseBody(); // retorna um objeto do tipo UserResponseDto
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        // Invalid username format
+        responseBody = testClient
+                .post()
+                .uri("/api/v1/users/save")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDto("tody@gmail", "123456"))
+                .exchange() // response
+                .expectStatus().isEqualTo(422) // status code 422
+                .expectBody(ErrorMessage.class) // tipo do objeto retornado
+                .returnResult().getResponseBody(); // retorna um objeto do tipo UserResponseDto
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+    }
+
+    @Test // Password validation test
+    public void createUser_InvalidPassword_ReturnErrorMessageStatus422(){
+        // Empty password
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/users/save")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDto("tody@gmail.com", ""))
+                .exchange() // response
+                .expectStatus().isEqualTo(422) // status code 422
+                .expectBody(ErrorMessage.class) // tipo do objeto retornado
+                .returnResult().getResponseBody(); // retorna um objeto do tipo UserResponseDto
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        // Password less than 6
+        responseBody = testClient
+                .post()
+                .uri("/api/v1/users/save")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDto("tody@gmail.com", "123"))
+                .exchange() // response
+                .expectStatus().isEqualTo(422) // status code 422
+                .expectBody(ErrorMessage.class) // tipo do objeto retornado
+                .returnResult().getResponseBody(); // retorna um objeto do tipo UserResponseDto
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        // Password greater than 6
+        responseBody = testClient
+                .post()
+                .uri("/api/v1/users/save")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDto("tody@gmail.com", "123456789"))
+                .exchange() // response
+                .expectStatus().isEqualTo(422) // status code 422
+                .expectBody(ErrorMessage.class) // tipo do objeto retornado
+                .returnResult().getResponseBody(); // retorna um objeto do tipo UserResponseDto
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+    }
 }
