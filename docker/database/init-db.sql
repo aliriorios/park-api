92dbb942b47ec072b8f34638726f2f7d840f9621
Alirio Rios <alirioriosmc@gmail.com>
2025-03-17 20:32:51 -0300

    Revendo configuracoes de banco de teste

--- docker/database/init-db.sql
+++ docker/database/init-db.sql
@@ -1,5 +1,8 @@
 -- Granting permissions
 GRANT ALL PRIVILEGES ON `park-api-db`.* TO 'alirio-dev'@'%';
+
+-- DATABASE TO TEST
+CREATE DATABASE IF NOT EXISTS `park-api-db-test`;
 GRANT ALL PRIVILEGES ON `park-api-db-test`.* TO 'alirio-dev'@'%';
 
 -- Apply
